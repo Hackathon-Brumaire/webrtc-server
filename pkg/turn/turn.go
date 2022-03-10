6fffd940cc82a19963c31e709efea24b4852aff7
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-03-10 10:27:46 +0100

    update public ip config

--- pkg/turn/turn.go
+++ pkg/turn/turn.go
@@ -16,7 +16,7 @@ type TurnServerConfig struct {
 
 func DefaultConfig() TurnServerConfig {
 	return TurnServerConfig{
-		PublicIP: "127.0.0.1",
+		PublicIP: "149.91.80.63",
 		Port:     19302,
 		Realm:    "flutter-webrtc",
 	}
